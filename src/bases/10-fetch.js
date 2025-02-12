828a97e94183a7f7068ea47c4eb43cbc05ddd3a7
Vanessa Dugarte <dugartevanessa@gmail.com>
2025-02-12 16:06:49 -0300

    fetch api

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,15 @@
+const apiKey =  'OHLxZzT4AsX0YjLT5DkIq9wtLxpc0SUT';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+    .then(resp => resp.json() )
+    .then( ({data}) => {
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+    })
+.catch (console.warn);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const apiKey =  'OHLxZzT4AsX0YjLT5DkIq9wtLxpc0SUT';
-
-const peticion = fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`);
-
-peticion.then(resp => console.log(resp));
